54fce9c88ef440de4f169d9fb126c3a234f6253e
Mohammed Zayan <zayanit@gmail.com>
2021-07-18 13:42:11 +0300

    Add route for books
    Lesson 3
    Endpoints and Payloads
    5. Flask - Route Decorator and Pagination

--- 1_Requests_Starter/backend/flaskr/__init__.py
+++ 1_Requests_Starter/backend/flaskr/__init__.py
@@ -8,11 +8,13 @@ from models import setup_db, Book
 
 BOOKS_PER_SHELF = 8
 
-# @TODO: General Instructions
-#   - As you're creating endpoints, define them and then search for 'TODO' within the frontend to update the endpoints there. 
-#     If you do not update the endpoints, the lab will not work - of no fault of your API code! 
-#   - Make sure for each route that you're thinking through when to abort and with which kind of error 
-#   - If you change any of the response body keys, make sure you update the frontend to correspond. 
+def paginate_books(request, allBooks):
+  page = request.args.get('page', 1, type=int)
+  start = (page - 1) * BOOKS_PER_SHELF
+  end = start + BOOKS_PER_SHELF
+  books = [book.format() for book in allBooks]
+  current_books = books[start:end]
+  return current_books
 
 def create_app(test_config=None):
   # create and configure the app
@@ -27,15 +29,19 @@ def create_app(test_config=None):
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
 
-  # @TODO: Write a route that retrivies all books, paginated. 
-  #         You can use the constant above to paginate by eight books.
-  #         If you decide to change the number of books per page,
-  #         update the frontend to handle additional books in the styling and pagination
-  #         Response body keys: 'success', 'books' and 'total_books'
-  # TEST: When completed, the webpage will display books including title, author, and rating shown as stars
+  @app.route('/books')
+  def retrieve_books():
+    allBooks = Book.query.order_by(Book.id).all()
+    current_books = paginate_books(request, allBooks)
 
+    if len(current_books) == 0:
+      abort(404)
 
-  
+    return jsonify({
+      'success': True,
+      'books': current_books,
+      'total_books': len(Book.query.all())
+    })
 
   # @TODO: Write a route that will update a single book's rating. 
   #         It should only be able to update the rating, not the entire representation
