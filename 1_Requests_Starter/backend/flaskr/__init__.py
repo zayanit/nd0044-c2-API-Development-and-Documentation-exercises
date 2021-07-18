767a20667d250e0fc1787cd22170b051ec099cf4
Mohammed Zayan <zayanit@gmail.com>
2021-07-18 13:59:19 +0300

    Add route to create a new book
    Lesson 3
    Endpoints and Payloads
    5. Flask - Route Decorator and Pagination

--- 1_Requests_Starter/backend/flaskr/__init__.py
+++ 1_Requests_Starter/backend/flaskr/__init__.py
@@ -89,10 +89,30 @@ def create_app(test_config=None):
       abort(422)
 
 
-  # @TODO: Write a route that create a new book. 
-  #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
-  # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books. 
-  #       Your new book should show up immediately after you submit it at the end of the page. 
+  @app.route('/books', methods=['POST'])
+  def create_book():
+    body = request.get_json()
+
+    new_title = body.get('title', None)
+    new_author = body.get('author', None)
+    new_rating = body.get('rating', None)
+
+    try:
+      book = Book(title=new_title, author=new_author, rating=new_rating)
+      book.insert()
+
+      allBooks = Book.query.order_by(Book.id).all()
+      current_books = paginate_books(request, allBooks)
+
+      return jsonify({
+        'success': True,
+        'created': book.id,
+        'books': current_books,
+        'total_books': len(Book.query.all())
+      })
+
+    except:
+      abort(422)
   
   return app
 
